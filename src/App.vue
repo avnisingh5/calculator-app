6b3cecb7fae576fd5fe645ec2f5b7154b66ecc70
avni singh <lalibgs721@gmail.com>
2023-07-27 20:21:46 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Calculator</h1>
+  <div>
+    <input type="number" v-model.number="firstNum"/>+
+    <input type="number" v-model.number="secondNum"/>=
+    <span>{{firstNum + secondNum}}
+
+    </span>
+  </div>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      firstNum:"",
+      secondNum:""
+    }
+  }
+ 
+ 
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
